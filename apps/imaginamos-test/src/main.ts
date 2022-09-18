8db248c29168442cf11e6de643a5e0a99c0604c9
erazo76 <erazo.gustavo@gmail.com>
2022-09-18 16:05:15 -0500

    Include and implement swagger

--- apps/imaginamos-test/src/main.ts
+++ apps/imaginamos-test/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,15 @@ async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);  
     app.useGlobalPipes(new ValidationPipe ());
+
+    const config = new DocumentBuilder()
+    .setTitle('Imaginamos-test Api')
+    .setDescription('Test pre-selección para Imaginamos, S.A.S')
+    .setVersion('1.0')    
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
     const server = await app.listen(PORT, async () => {
       Logger .log(`Imaginamos-api Server is running in port ${PORT}`);     
     });    
